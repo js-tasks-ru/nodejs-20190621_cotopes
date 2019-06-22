9a27c758221da0202b6cac9f99d881bdf6de96b5
niarlatotep <caimac@yandex.ru>
2019-06-22 10:41:36 +0300

    2-module 1-task

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,10 +4,17 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.dataLength = 0;
+    this.limit = options.limit;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.dataLength += chunk.length;
+    if (this.dataLength >= this.limit) {
+      callback(new LimitExceededError(), chunk);
+    }
+    this.push(chunk);
+    callback();
   }
 }
 
