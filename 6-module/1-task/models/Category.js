008b79d28b8b1a5542b4da42fb908af37c574889
niarlatotep <caimac@yandex.ru>
2019-07-10 22:41:08 +0300

    6-module 1-task

--- 6-module/1-task/models/Category.js
+++ 6-module/1-task/models/Category.js
@@ -2,10 +2,22 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const subCategorySchema = new mongoose.Schema({
+  title: {type: String, required: true},
+});
+subCategorySchema.virtual('products', {
+  ref: 'Product',
+  localField: '_id',
+  foreignField: 'subcategory'
 });
 
 const categorySchema = new mongoose.Schema({
+  title: {type: String, required: true},
   subcategories: [subCategorySchema]
+});
+categorySchema.virtual('products', {
+  ref: 'Product',
+  localField: '_id',
+  foreignField: 'category'
 });
 
 module.exports = connection.model('Category', categorySchema);
